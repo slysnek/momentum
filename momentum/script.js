4a38bdfdc6b354b6c0e1c2d43c3b5a50c2dec244
Kirill <bloodwingest@gmail.com>
2022-08-10 17:43:42 +0300

    feat: added clock and calendar

--- momentum/script.js
+++ momentum/script.js
@@ -1,20 +1,33 @@
-const clock = document.querySelector('.clock')
-const calendar = document.querySelector('.calendar')
-
-const date = new Date()
-const currentTime = date.toLocaleTimeString();
-const currentDate = date.toLocaleDateString();
-
-function showTime(){
-    const date = new Date()
-    const currentTime = date.toLocaleTimeString();
-    const currentDate = date.toLocaleDateString();
-    clock.textContent = currentTime;
-    calendar.textContent = currentDate;
-
-    console.log(date);
-    console.log(currentTime);
-    setTimeout(showTime, 1000)
+const clock = document.querySelector('.clock');
+const calendar = document.querySelector('.calendar');
+const greeting = document.querySelector('.greeting')
+//время и календарь
+function showTimeandDate(){
+    const date = new Date();
+    const options = {month: 'long', day:'numeric', weekday: 'long'};
+    clock.textContent = date.toLocaleTimeString();
+    calendar.textContent = date.toLocaleDateString('ru-RU', options);
+    setTimeout(showTimeandDate, 1000)
+}
+//приветствие
+function greetTheUser(){
+    const date = new Date();
+    const hours = date.getHours();
+    const timeOfDay = getTimeOfDay(hours)
+    greeting.textContent = `Good ${timeOfDay},`
+    setTimeout(greetTheUser, 1000)
+}
+function getTimeOfDay(hours){
+    if(hours >= 6 && hours < 12){
+        return 'Morning'
+    } else if(hours >= 12 && hours < 18){
+        return 'Afternoon'
+    } else if(hours >= 18 && hours < 24){
+        return 'Evening'
+    } else if(hours >= 0 && hours < 6){
+        return 'Night'
+    }
 }
 
-showTime();
+showTimeandDate();
+greetTheUser();
